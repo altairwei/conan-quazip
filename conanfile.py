dc089ee3914f9f7de19ea11b6d14edb7212259eb
Altair Wei <altair_wei@outlook.com>
2018-12-12 15:47:35 +0800

    first commit for conan-quazip

---
+++ conanfile.py
@@ -0,0 +1,41 @@
+from conans import ConanFile, CMake, tools
+
+
+class QuazipConan(ConanFile):
+    name = "quazip"
+    version = "0.7.6"
+    license = "LGPL-2.1, zlib/png"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "A Qt/C++ wrapper for Gilles Vollant's ZIP/UNZIP C package (minizip). Provides access to ZIP archives from Qt programs using QIODevice API."
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=True"
+    generators = "cmake"
+
+    def source(self):
+        self.run("git clone https://github.com/stachenov/quazip.git")
+        self.run("cd quazip && git checkout 0.7.6")
+        # This small hack might be useful to guarantee proper /MT /MD linkage
+        # in MSVC if the packaged project doesn't have variables to set it
+        # properly
+        tools.replace_in_file("quazip/CMakeLists.txt", "project(QuaZip)",
+                              '''project(QuaZip)
+include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+conan_basic_setup()''')
+
+    def build(self):
+        cmake = CMake(self)
+        shared = "-DBUILD_SHARED_LIBS=ON" if self.options.shared else ""
+        self.run('cmake quazip %s %s' % (cmake.command_line, shared))
+        self.run("cmake --build . %s" % cmake.build_config)
+
+    def package(self):
+        self.copy(src="quazip/quazip", pattern="*.h", dst="include/quazip")
+        self.copy(pattern="*quazip.lib", dst="lib", keep_path=False)
+        self.copy(pattern="*.dll", dst="bin", keep_path=False)
+        self.copy(pattern="*.so*", dst="lib", keep_path=False)
+        self.copy(pattern="*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["quazip5d"] if (self.settings.os == "Windows" and self.settings.build_type == "Debug") else ["quazip5"]
+
