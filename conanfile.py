a6550c406f6f23165a449ea9d78fb1d49d0e3575
Altair Wei <altair_wei@outlook.com>
2018-12-16 18:06:28 +0800

    take full control of CMakeLists.txt in quazip/quazip

--- conanfile.py
+++ conanfile.py
@@ -16,7 +16,7 @@ class QuazipConan(ConanFile):
         "shared": False,
         "zlib:shared": False
     }
-    exports_sources = ["CMakeLists.txt"]
+    exports_sources = ["CMakeLists.txt", "CMakeLists-2.txt"]
     generators = "cmake_find_package"
     _source_subfolder = "source_subfolder"
 
@@ -24,20 +24,28 @@ class QuazipConan(ConanFile):
         url = 'https://github.com/stachenov/quazip/archive/%s.tar.gz' % self.version
         tools.get(url)
         os.rename("quazip-" + self.version, self._source_subfolder)
+        # toplevel CMakeLists.txt
         os.rename(os.path.join(self._source_subfolder, "CMakeLists.txt"),
                   os.path.join(self._source_subfolder, "CMakeListsOriginal.txt"))
         shutil.copy("CMakeLists.txt",
                     os.path.join(self._source_subfolder, "CMakeLists.txt"))
+        # second level CMakeLists.txt
+        os.rename(os.path.join(self._source_subfolder, "quazip", "CMakeLists.txt"),
+                  os.path.join(self._source_subfolder, "quazip","CMakeListsOriginal.txt"))
+        shutil.copy("CMakeLists-2.txt",
+                    os.path.join(self._source_subfolder, "quazip","CMakeLists.txt"))
 
     def build(self):
         cmake = CMake(self)
+        cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
+        cmake.definitions["QUAZIP_STATIC"] = not self.options.shared
         if self.settings.os == "Windows":
-            cmake.definitions["CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS"] = 1
+            cmake.definitions["CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS"] = self.options.shared
         cmake.configure(source_folder=self._source_subfolder)
         cmake.build()
 
-    def build_id(self):
-        self.info_build.options.shared = "shared_and_static"
+    #def build_id(self):
+        #self.info_build.options.shared = "shared_and_static"
 
     def package(self):
         self.copy(pattern="COPYING", dst="licenses", src=self._source_subfolder)
@@ -45,21 +53,22 @@ class QuazipConan(ConanFile):
             src=os.path.join(self._source_subfolder, "quazip"))
         if self.options.shared:
             self.copy(pattern="*.dll", dst="bin", keep_path=False)
-            self.copy(pattern="*quazip5.lib", dst="bin", keep_path=False)
-            self.copy(pattern="*quazip5.exp", dst="bin", keep_path=False)
+            self.copy(pattern="*.lib", dst="bin", keep_path=False)
             self.copy(pattern="*.so*", dst="lib", keep_path=False)
             self.copy(pattern="*.dylib*", dst="lib", keep_path=False)
+        #     self.copy(pattern="*.dll", dst="bin", keep_path=False)
+        #     self.copy(pattern="*quazip5*.lib", dst="bin", keep_path=False)
+        #     self.copy(pattern="*quazip5*.exp", dst="bin", keep_path=False)
+        #     self.copy(pattern="*.so*", dst="lib", keep_path=False)
+        #     self.copy(pattern="*.dylib*", dst="lib", keep_path=False)
         else:
-            self.copy(pattern="*quazip_static.lib", dst="lib", keep_path=False)
+            self.copy(pattern="*.lib", dst="lib", keep_path=False)
             self.copy(pattern="*.a", dst="lib", keep_path=False)
+        #     self.copy(pattern="*quazip5.exp", dst="lib", keep_path=False)
+        #     self.copy(pattern="*quazip_static*.lib", dst="lib", keep_path=False)
+        #     self.copy(pattern="*.a", dst="lib", keep_path=False)
 
     def package_info(self):
         if self.options.shared:
             self.cpp_info.libdirs.append("bin")
         self.cpp_info.libs = tools.collect_libs(self)
-        # if self.settings.build_type == "Debug":
-        #     self.cpp_info.libs.append("quazip5d")
-        # else:
-        #     self.cpp_info.libs.append("quazip5")
-        #     if not self.options.shared:
-        #         self.cpp_info.libs.append("quazip_static")
